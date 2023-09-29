fede7bd8d2635fb9b7708655dcfdad14722ce921
Ayush Bisht <90270129+Ayush-singhBisht@users.noreply.github.com>
2023-09-29 20:52:24 +0530

    Add files via upload

---
+++ 01_hello.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // #include <iostream>
+// // using namespace std;
+
+// // int main() {
+// //     string s1="str",s2="pk";
+// //     cout<<s1+s2;
+// //     return 0;
+// // }
+
+
+// // class Solution {
+// // public:
+// //     string truncateSentence(string s, int k) {
+        
+// //     int count=0;
+// //     string ans="";
+// //     for(int i=0;i<s.size();i++)
+// //     {
+// //         if(s[i]==' ')
+// //             count++;
+// //         if(count==k)
+// //             break;
+// //         ans=ans+s[i];
+// //     }
+// //     return ans;
+
+
+// //     }
+// // };
+
+
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int main() {
+//   vector<vector<int>> grid = {{1,2,4},{3,3,1}};
+
+//   // Accessing values in the 2D vector
+//   cout << grid[0][0] << endl; // prints 1
+//   cout << grid[0][1] << endl; // prints 2
+//   cout << grid[0][2] << endl; // prints 4
+//   cout << grid[1][0] << endl; // prints 3
+//   cout << grid[1][1] << endl; // prints 3
+//   cout << grid[1][2] << endl; // prints 1
+
+//   return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[5][5] = {
+//         {0, 1, 2, 3, 4},
+//         {5, 6, 7, 8, 9},
+//         {10, 11, 12, 13, 14},
+//         {15, 16, 17, 18, 19},
+//         {20, 21, 22, 23, 24}
+//     };
+    
+//     cout << arr[0][4] << endl;
+    
+//     return 0;
+// }
