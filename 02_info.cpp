dd92262a96686efcce1125e19284a95804c562d5
Ayush Bisht <90270129+Ayush-singhBisht@users.noreply.github.com>
2023-09-29 20:53:53 +0530

    Add files via upload

---
+++ 02_info.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"NAME = "<<"Ayush Petwal "<<endl;
+    cout<<"AGE = "<<"19yrs "<<endl;
+    cout<<"GENDER = "<<"Male "<<endl;
+    cout<<"ADDRESS = "<<"Dehradun "<<endl;
+    return 0;
+}
